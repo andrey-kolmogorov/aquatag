e5d69b824e804d0d62ea2a770a0c1bc34b85b9e6
Andrei Kolmogorov <ano@vizrt.com>
2026-04-05 14:21:28 +0200

    Initial Commit

---
+++ AquaTag/AquaTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaTagApp.swift
+//  AquaTag
+//
+//  Created by Andrei Kolmogorov on 05.04.26.
+//
+
+import SwiftUI
+
+@main
+struct AquaTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AquaTag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AquaTag
+//
+//  Created by Andrei Kolmogorov on 05.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
